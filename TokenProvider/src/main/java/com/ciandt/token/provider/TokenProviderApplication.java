9a29161adfbe347320726ebd029e1125b7a6ea92
brunomartins26 <109685872+brunomartins26@users.noreply.github.com>
2022-07-22 20:06:07 -0300

    Add files via upload

---
+++ TokenProvider/src/main/java/com/ciandt/token/provider/TokenProviderApplication.java
@@ -0,0 +1,15 @@
+package com.ciandt.token.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Teste
+
+@SpringBootApplication
+public class TokenProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenProviderApplication.class, args);
+	}
+
+}
