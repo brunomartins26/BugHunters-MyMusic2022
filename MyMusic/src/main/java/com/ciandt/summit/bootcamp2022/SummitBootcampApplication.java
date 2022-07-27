9c9680776ad3bb71cdd2ff15e84585be009b4354
hyginocit <gnieves@ciandt.com>
2022-07-27 13:46:39 -0300

    corrigindo algumas alterações no repository

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/SummitBootcampApplication.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/SummitBootcampApplication.java
@@ -1,21 +1,21 @@
 package com.ciandt.summit.bootcamp2022;
 
-import com.ciandt.summit.bootcamp2022.repository.ArtistasRepository;
-import com.ciandt.summit.bootcamp2022.repository.MusicasRepository;
-import org.springframework.boot.CommandLineRunner;
+import com.ciandt.summit.bootcamp2022.repository.ArtistaRepository;
+import com.ciandt.summit.bootcamp2022.repository.MusicaRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SummitBootcampApplication{
 
-	private final MusicasRepository musicasRepository;
-	private final ArtistasRepository artistasRepository;
+	private final MusicaRepository musicaRepository;
+	private final ArtistaRepository artistaRepository;
 
-	public SummitBootcampApplication(MusicasRepository musicasRepository, ArtistasRepository artistasRepository){
-		this.musicasRepository = musicasRepository;
-		this.artistasRepository = artistasRepository;
+	public SummitBootcampApplication(MusicaRepository musicaRepository, ArtistaRepository artistaRepository){
+		this.musicaRepository = musicaRepository;
+		this.artistaRepository = artistaRepository;
 	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SummitBootcampApplication.class, args);
 	}
--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/repository/ArtistaRepository.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/repository/ArtistaRepository.java
@@ -1,12 +1,13 @@
 package com.ciandt.summit.bootcamp2022.repository;
 
+import com.ciandt.summit.bootcamp2022.entity.Artista;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface ArtistasRepository extends JpaRepository<Artistas, String>{
+public interface ArtistaRepository extends JpaRepository<Artista, String>{
 
-    List<Artistas> findByNomeContainingIgnoreCase(String nome);
+    List<Artista> findByNomeContainingIgnoreCase(String nome);
 
 }
