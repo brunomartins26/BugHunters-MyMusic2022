c787bb352a346ad86b2dde69300c5d16c7c6b8d7
hyginocit <gnieves@ciandt.com>
2022-07-28 15:04:33 -0300

    QUERY no Repository

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/repository/MusicaRepository.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/repository/MusicaRepository.java
@@ -1,17 +1,17 @@
 package com.ciandt.summit.bootcamp2022.repository;
 
-import com.ciandt.summit.bootcamp2022.entity.Artista;
 import com.ciandt.summit.bootcamp2022.entity.Musica;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 
 @Repository
 public interface MusicaRepository extends JpaRepository<Musica, String> {
-
-    List<Musica> findByNomeContainsIgnoreCaseOrArtista_NomeContainsIgnoreCaseAllIgnoreCaseOrderByArtista_NomeAscNomeAsc(String nome, String nome1);
-
+    @Query("SELECT m FROM Musica m JOIN Artista a on a.id = m.artista WHERE a.nome like '%'|| :filtro ||'%' or m.nome like '%'|| :filtro ||'%' ORDER by a.nome, m.nome asc")
+    List<Musica> buscarMusicaArtista(@Param("filtro") String filtro);
     }
 
 
