4b5aa6bc2b59aad656192ae438831db9095dceb9
alepereiraciandt <alexandrepereira@ciandt.com>
2022-08-02 16:27:14 -0300

    refatoracao depois da camis

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/MusicController.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/MusicController.java
@@ -1,6 +1,7 @@
 package com.ciandt.summit.bootcamp2022.controller;
 
 import com.ciandt.summit.bootcamp2022.service.MusicService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
